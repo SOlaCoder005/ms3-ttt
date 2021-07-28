e969dbb3556e1326c477d8f4b52b2bfa1a9f0cb2
SOlaCoder005 <misssamfelicia@gmail.com>
2021-07-28 12:53:50 +0000

    added additional .py file to repo

---
+++ study-notes.py
@@ -0,0 +1,8 @@
+# Ref: Dawson, 2010 pp. 169-70 
+
+def birthday1(name, age):
+ 
+    print(f"Happy birthday ,", name, "! You are ", age, " today.\n" )
+
+# birthday1(name = "Jack", age = 4) | Happy birthday , Jack ! You are  4  today. | Default values
+# birthday1("Jack", 4) | Happy birthday , Jack ! You are  4  today. | Positional Parameters
---
+++ t-three-game.py
@@ -0,0 +1,46 @@
+# T*3 (T to the Power 3)
+# This is a tic-tac-toe game. The user plays against the computer in a game of tic-tac-toe.
+
+# Template code
+
+    # def fucntion_template():
+    #     """
+    #     Doc String
+    #     """
+    #     print()
+
+    # fucntion_template()
+
+def welcome_message():
+    """
+    This message greets the user when they initially run the game.
+    """
+    print("Hello, and Welocome to the T*3 game!\n")
+    print("Basically, T*3 is a game of Tic-Tac-Toe.\n")
+    print("This is a place where great minds meet to engage in digital strategic sparing!\n")
+    print("This is one of the last places where you are able to put AI in it's place and show it whos boss.\n")
+    print("Are you ready to play?")
+
+welcome_message()
+
+
+def game_guides():
+    """
+    This message tell the user how to play the game
+    """
+    print(
+        """ 
+        Your battle-ground is formed of 9 squares and looks like this: 
+
+                            0  |  1  |  2
+                            --------------
+                            3  |  4  |  5
+                            --------------
+                            6  |  7  |  9
+
+    
+        """
+    )
+   
+
+game_guides()
