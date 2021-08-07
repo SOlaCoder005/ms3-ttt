79ea7e83ce058442504da7db7084de44fc6f935b
SOlaCoder005 <misssamfelicia@gmail.com>
2021-08-07 10:14:40 +0000

    created board as a class rather than function

--- ttt.py
+++ ttt.py
@@ -6,18 +6,22 @@ Imported Libraries and Modules
 import emoji # Credit: Unicode
 import time # Credit: Guru99
 import random
+# https://www.youtube.com/watch?v=7Djh-Cbgi0E
+import os
+os.system("clear")
 
 
 """
 CONST variables
 """
-X = 0
-O = 0
+
+X = ()
+O = ()
 PlayerWon = ""
 PlayerLost = ""
 PlayerTie = ""
-BOARD_SQUARES = 9
-BLANK_SPACE = " "
+# BOARD_SQUARES = 9
+# BLANK_SPACE = " "
 GAMEPIECES = {
     "a":"\U0001F9DE", #mermaid
     "b":"\U0001F9D1", #cook
@@ -30,22 +34,27 @@ GAMEPIECES = {
     "i":"\U0001F4A3", #bomb          
 }
 
-
 def welcome():
+
     """
     Greets user when they initially run the game.
     """
+
     print("\nWELCOME TO TIC-TAC-TOE! \U0001F609\n")
     print("This is a place to engage in digital strategic sparing!\n")
     print("Can you put AI in it's place to show it who's boss?\n")
     time.sleep(2)
     print("I hope so...\n")
     time.sleep(3)
+    # return r_u_ready_to_play()
 
+#r u ready to play no function needs fixing
 def r_u_ready_to_play():
+
     """
     Asks user if they are ready to play.
     """
+
     yes = "y"
     no = "n" 
     
@@ -74,9 +83,11 @@ def r_u_ready_to_play():
             )
  
 def game_guides():
+    
     """
     Tells user how to play the game.
     """
+
     print(
     
         """
@@ -118,9 +129,12 @@ def game_pieces():
     )
     print(
         """
-            For instance: 
-            -------------
-        
+        \U0001F4DC For Instance:
+        -----------------------
+        """
+    )
+    print(
+        """
                             a = \U0001F9DE 
                             b = \U0001F9D1
                             c = \U0001F444
@@ -137,17 +151,17 @@ def game_pieces():
     print(
         # Do not chnage spacing as this aligns eefectivily in the terminal
         """
-                        a  |  b  | c  
-                        -------------
-                        \U0001F9DE | \U0001F9D1  | \U0001F444
+                            a  |  b  | c  
+                            -------------
+                            \U0001F9DE | \U0001F9D1  | \U0001F444
 
-                        d  |  e  | f  
-                        -------------
-                        \U0001F63B | \U0001F47B  | \U0001F916
+                            d  |  e  | f  
+                            -------------
+                            \U0001F63B | \U0001F47B  | \U0001F916
 
-                        g  |  h  | i  
-                        -------------
-                        \U0001F47D | \U0001F9E0  | \U0001F4A3
+                            g  |  h  | i  
+                            -------------
+                            \U0001F47D | \U0001F9E0  | \U0001F4A3
 
         """
     )
@@ -162,13 +176,15 @@ def human_picks_game_piece():
 
     if human_game_piece_picked in GAMEPIECES:
         print(f"\nOkay, your game piece is '{ GAMEPIECES[human_game_piece_picked] }'.\n")
+        X = GAMEPIECES[human_game_piece_picked]
+
     else:  
         print(f"\nSorry, didn't recognise yourt respnse '{ human_game_piece_picked }'.\n")
         return human_picks_game_piece()
         time.sleep(1)
-
-           
+         
 def ai_picks_game_piece():
+    #NEED TO DO A LOOP SO AI DOES NOT PICK THE SAME PIECE AS PLAYER
 
     """
     - Allows computer ('AI') to select game piece.
@@ -178,49 +194,59 @@ def ai_picks_game_piece():
     games_piece_list = list(GAMEPIECES.values())
     ai_game_piece_picked = random.choice(games_piece_list)
     print(f"\nGreat, the AI has picked '{ai_game_piece_picked}' as it's game piece.\n")
+    O = ai_game_piece_picked
     time.sleep(1)
     print("\nOkay let's play! \U0001F60E\n")
     time.sleep(1.5)
-    return board_structure()
+    # return board_structure()
 
+# https://www.youtube.com/watch?v=7Djh-Cbgi0E
+class Board():
 
-
-def board_structure():
-    """
-    - Loads new game board
-    - This code has been adapted from Dawson (2010, p.180)
-    """
-    print(
-        """
-        Here's the board below:
-        """
-    )
-    board = []  
-    for square in range(BOARD_SQUARES):
-        board.append(BLANK_SPACE)
-    return board
-
-
-def load_game_board(board):
-
+    def __init__(self):
+        self.cells = [" ", " ", " " , " " , " " , " " , " " , " " , " " , " " ]
+    
+    def board_structure(self):
         print("\n")
-        print("\n\t", board[0],       "|", board[1],         "|", board[2])
-        print("\t", "------------------")
-        print("\t", board[3],         "|", board[4],         "|", board[5])
-        print("\t", "------------------")
-        print("\t", board[6],         "|", board[7],         "|", board[8])
+        print(" %s | %s | %s " %(self.cells[1], self.cells[2], self.cells[3]))
+        print("-----------")
+        print(" %s | %s | %s " %(self.cells[4], self.cells[5], self.cells[6]))
+        print("-----------")
+        print(" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
         print("\n")
 
 
+board = Board()
+board.board_structure()
 
+# def empty_board():
+#     """
+#     Initialises new game board
+#     Source: Dawson (2010, p.180)
+#     """
+#     board = []
+#     for square in range (BOARD_SQUARES):
+#         board.append(BLANK_SPACE)
+#     return board
+    
+# def present_board(board):
 
+#     print("\n\t", board[0], "|", board[1], "|", board[2])
+#     print("\t", "---------")
+#     print("\t", board[3], "|", board[4], "|", board[5])
+#     print("\t", "---------")
+#     print("\t", board[6], "|", board[7], "|", board[8], "\n")
+      
 
+# def game_moves(board):
 
+#     moves = []
+#     for square in range(BOARD_SQUARES):
+#         if board[square] == BLANK_SPACE:
+#             moves.append(square)
+#     return moves    
 
-
-
-
-
+# game_moves()
 
 
 
@@ -228,24 +254,31 @@ def load_game_board(board):
 
 
 def exit():
+
     print(input("\nPlease press ENTER on your keyboard to exit:\n"))
     time.sleep(.5) 
     print("Exiting Game mode...\n") 
     time.sleep(1)
+    #NEED to clear the terminal once exiting
     return welcome()
-
     
-def main(): 
-    """
-    Contains key functions that allows the game to run. 
-    """
-    welcome()
-    r_u_ready_to_play()
-    game_pieces()
-    human_picks_game_piece()
-    ai_picks_game_piece()
-    board = board_structure()
-    load_game_board(board)
-    exit()
-main()
+
+# def cls():
+#     os.system('cls' if os.name == 'nt' else 'clear')
+# cls()
+   
+# def main(): 
+#     """
+#     Contains key functions that allows the game to run. 
+#     """
+#     # welcome()
+#     # r_u_ready_to_play()
+#     # game_pieces()
+#     # human_picks_game_piece()
+#     # ai_picks_game_piece()
+#     # board = empty_board() 
+#     # present_board(board)
+#     # game_moves()
+#     # exit() 
+# main()
 
