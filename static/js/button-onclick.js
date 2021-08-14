580f3795d1568003656598b5207b8ea4bb0e7802
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-08-14 09:22:59 +0100

    restored button settings
    external .js attempt failed

--- static/js/button-onclick.js
+++
@@ -1,7 +0,0 @@
-const playBtnRef = document.querySelector("#play-btn");
-
-function playBtnRef() {
-    onclick = window.location.reload();
-}
-
-playBtnRef.addEventListener("click", onclick, false);
